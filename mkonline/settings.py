2a1c64c10ae67abe58c1bfcd77c564fd53957067
cannon-liu <woliuliwen@163.com>
2018-09-18 14:17:50 +0800

    Update settings.py

--- mkonline/settings.py
+++ mkonline/settings.py
@@ -107,10 +107,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mkonline',
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'USER': '',
+        'PASSWORD': '',
         # 'HOST': '127.0.0.1',
-        'HOST': '111.231.119.133',
+
 
     }
 }
